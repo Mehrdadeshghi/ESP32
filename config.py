cb5175873593c452d986b85e10d3506109a66138
Mehrdadeshghi <158504801+Mehrdadeshghi@users.noreply.github.com>
2024-09-15 23:50:01 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,13 @@
 import os
 
 class Config:
-    # Secret Key für Flask-Anwendung
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'DEIN_SECRET_KEY')
-
-    # Datenbankkonfiguration
-    DB_USER = os.environ.get('DB_USER', 'DEIN_DB_BENUTZER')
-    DB_PASSWORD = os.environ.get('DB_PASSWORD', 'DEIN_DB_PASSWORT')
-    DB_HOST = os.environ.get('DB_HOST', 'localhost')  # Oder die IP-Adresse deines Datenbankservers
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'mysecretkey123')
+    DB_USER = os.environ.get('DB_USER', 'esp32_user')
+    DB_PASSWORD = os.environ.get('DB_PASSWORD', 'password123')
+    DB_HOST = os.environ.get('DB_HOST', 'localhost')
     DB_NAME = os.environ.get('DB_NAME', 'esp32_db')
 
     SQLALCHEMY_DATABASE_URI = f"mysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # API-Schlüssel für die Authentifizierung
-    API_KEY = os.environ.get('API_KEY', 'DEIN_API_KEY')
+    API_KEY = os.environ.get('API_KEY', '1234567890abcdef1234567890abcdef')
